fce9686cc808416f6723d0c47482193115d71b00
Yabin Cui <splintcoder@gmail.cm>
2017-01-21 17:00:09 -0800

    Add IncreasingSubsequences, TargetSum.

---
+++ leetcode/IncreasingSubsequences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> path;
+        recur(-200, 0, nums, path, result);
+        return result;
+    }
+    
+    void recur(int lastValue, int curIndex, const vector<int>& nums, vector<int>& path, vector<vector<int>>& result) {
+        if (curIndex == nums.size()) {
+            return;
+        }
+        unordered_map<int, bool> used;
+        for (int i = curIndex; i < nums.size(); ++i) {
+            if (nums[i] >= lastValue && used.find(nums[i]) == used.end()) {
+                used[nums[i]] = true;
+                path.push_back(nums[i]);
+                if (path.size() > 1) {
+                    result.push_back(path);
+                }
+                recur(nums[i], i + 1, nums, path, result);
+                path.pop_back();
+            }
+        }
+    }
+};
---
+++ leetcode/TargetSum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int total = 0;
+        for (auto& v : nums) {
+            total += v;
+        }
+        int value_count = total * 2 + 1;
+        int dp[value_count];
+        int new_dp[value_count];
+        memset(dp, 0, sizeof(dp));
+        dp[total + 0] = 1;
+        for (auto& v : nums) {
+            memset(new_dp, 0, sizeof(new_dp));
+            // use positive v.
+            for (int i = 0; i < value_count; ++i) {
+                if (dp[i] != 0) {
+                    new_dp[i + v] += dp[i];
+                    new_dp[i - v] += dp[i];
+                }
+            }
+            memcpy(dp, new_dp, sizeof(dp));
+        }
+        int t = S + total;
+        if (t >= 0 && t < value_count) {
+            return dp[t];
+        }
+        return 0;
+    }
+};
