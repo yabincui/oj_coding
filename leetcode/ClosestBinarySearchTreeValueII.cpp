90a8552846e8124f8711915af1b11f431be27a02
Yabin Cui <splintcoder@gmail.cm>
2017-02-21 23:47:30 -0800

    Add NumberOfDigitOne.

---
+++ leetcode/ClosestBinarySearchTreeValueII.cpp
@@ -0,0 +1,84 @@
+// 272 Closest Binary Search Tree Value II
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        Deque<TreeNode> leftS = new ArrayDeque<TreeNode>();
+        Deque<TreeNode> rightS = new ArrayDeque<TreeNode>();
+        findNearest(root, target, leftS, true);
+        findNearest(root, target, rightS, false);
+        List<Integer> res = new ArrayList<Integer>();
+        while (res.size() < k) {
+            boolean use_left = false;
+            boolean use_right = false;
+            if (leftS.isEmpty()) {
+                use_right = true;
+            } else if (rightS.isEmpty()) {
+                use_left = true;
+            } else if (leftS.peek().val == rightS.peek().val) {
+                use_left = use_right = true;
+            } else if (target - leftS.peek().val < rightS.peek().val - target) {
+                use_left = true;
+            } else {
+                use_right = true;
+            }
+            res.add(use_left ? leftS.peek().val : rightS.peek().val);
+            if (use_left) {
+                TreeNode node = leftS.pop();
+                node = node.left;
+                while (node != null) {
+                    leftS.push(node);
+                    node = node.right;
+                }
+                while (!leftS.isEmpty() && leftS.peek().val > target) {
+                    leftS.pop();
+                }
+            }
+            if (use_right) {
+                TreeNode node = rightS.pop();
+                node = node.right;
+                while (node != null) {
+                    rightS.push(node);
+                    node = node.left;
+                }
+                while (!rightS.isEmpty() && rightS.peek().val < target) {
+                    rightS.pop();
+                }
+            }
+        }
+        return res;
+    }
+    
+    void findNearest(TreeNode root, double target, Deque<TreeNode> s, boolean fromLeft) {
+        while (root != null) {
+            s.push(root);
+            //System.out.printf("fromLeft = %b, push %d\n", fromLeft, root.val);
+            if (root.val == target) {
+                break;
+            }
+            if (root.val < target) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        if (fromLeft) {
+            while (!s.isEmpty() && s.peek().val > target) {
+                //System.out.printf("pop %d\n", s.peek().val);
+                s.pop();
+            }
+        } else {
+            while (!s.isEmpty() && s.peek().val < target) {
+                //System.out.printf("pop %d\n", s.peek().val);
+                s.pop();
+            }
+        }
+    }
+}
---
+++ leetcode/NumberOfDigitOne.cpp
@@ -0,0 +1,32 @@
+233. Number of Digit One.
+class Solution {
+public:
+    int countDigitOne(int n) {
+        if (n <= 0) return 0;
+        int res = 0;
+        int base = 1;
+        int max_level = 0;
+        while (n / base >= 10) {
+            base *= 10;
+            max_level++;
+        }
+        base = 1;
+        for (int level = 0; level <= max_level; ++level) {
+            // How many digit 1 at 10^level.
+            int value = base;
+            int left_count = n / value / 10;
+            int tmp = 0;
+            if (n / value % 10 == 1) {
+                tmp += n % value + 1;
+                tmp += left_count * value;
+            } else if (n / value % 10 == 0) {
+                tmp += left_count * value;
+            } else {
+                tmp += (left_count + 1) * value;
+            }
+            res += tmp;
+            base *= 10;
+        }
+        return res;
+    }
+};
